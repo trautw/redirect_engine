f0352e759a7d17d3e0cf364b7d1fb66aeb055952
Christoph Trautwein <trautw@ubuntu.skweg24.de>
2013-02-11 16:10:07 +0100

    restrukturierung

--- lib/redirect_engine.rb
+++ lib/redirect_engine.rb
@@ -1,4 +1,63 @@
-require "redirect_engine/version"
+require 'redirect_engine/version'
+require 'redirect_engine/engine'
+
+# set :public_felder, File.dirname(__FILE__) + '/public'
+
+# See http://www.sinatrarb.com/intro
+# set :environment, :development
+set :environment, :development
+set :port, 80
+# set :bind, "2001:6f8:966:ac10::106"
+# set :bind, "ubuntu.chtw.de"
+set :bind, "2001:470:1f15:5a8:20c:29ff:fe21:87aa"
+
+engine = RedirectEngine::Engine.new
+engine.start()
+
+# See http://www.sinatrarb.com/intro
+# set :environment, :development
+set :environment, :development
+set :port, 80
+# set :bind, "2001:6f8:966:ac10::106"
+# set :bind, "ubuntu.chtw.de"
+set :bind, "2001:470:1f15:5a8:20c:29ff:fe21:87aa"
+
+# ---------------------------------------------------
+before do
+  content_type :html, 'charset' => 'utf-8'
+  # See http://rack.rubyforge.org/doc/classes/Rack/Request.html
+
+  if request.env["HTTP_X_FORWARDED_HOST"]
+    request.path_info = "/redirector#{request.path_info}"
+  end
+  # write_log "Querystring = #{request.query_string}\n"
+  # write_log "Param source = #{request["source"]}\n"
+  # http://stackoverflow.com/questions/6317705/rackrequest-how-do-i-get-all-headers
+  # headers = env.select {|k,v| k.start_with? 'HTTP_'}
+  # write_log "Headers = #{headers}\n"
+  # write_log "Host = #{request.env["HTTP_HOST"]}\n"
+  # write_log "Host = #{headers["HTTP_HOST"]}\n"
+  # write_log "Original Host = #{request.env["HTTP_X_FORWARDED_HOST"]}\n"
+end
+
+after do
+  puts "Response status = #{response.status}"
+end
+
+helpers do
+  include Rack::Utils
+  alias_method :h, :escape_html
+end
+
+get '/redirector/:query' do
+  engine.redirect_for(self,  request, params)
+end
+
+get '/*' do
+  # write_log "Error: Request doesn't match a valid route!"
+  print "Error: Request #{params[:splat]} doesn't match a valid route!\n"
+  return "@@ERROR@@" + "Error: Request doesn't match a valid route!\n"
+end
 
 module RedirectEngine
   # Your code goes here...
---
+++ lib/redirect_engine/engine.rb
@@ -0,0 +1,82 @@
+require 'rubygems'
+require 'bundler/setup'
+
+class RedirectEngine::Engine
+
+  $config = Hash.new
+
+  def start()
+    begin
+      print "Hello from Engine\n"
+    end
+  end
+
+  def redirect_for(sina, request, params)
+    original_host = request.env["HTTP_X_FORWARDED_HOST"]
+    write_log "=================== Redirecting for #{original_host} #{params[:query]}"
+    write_log "Querystring = #{request.query_string}\n"
+
+    redirect_config = get_redirect_config(original_host)
+
+    write_log "Redirect Config = #{redirect_config}"
+    return_code  = redirect_config["default"]["return_code"]
+    redirect_url = redirect_config["default"]["target"]
+
+    write_log "Redirecting #{params[:query]}\n"
+
+    request_uri = "/#{params[:query]}"
+    request_params = "#{request.query_string}"
+
+    redirect_config["redirect"].each do |redirect|
+      write_log "Checking regexp #{redirect['source']}"
+      write_log "Request URI = #{request_uri}"
+      if request_uri.match(redirect['source'])
+        write_log "Hit!!"
+        redirect_url = "http://#{original_host}#{redirect['target']}"
+        return_code  = redirect["return_code"] if redirect["return_code"]
+      end
+    end
+
+    write_log "Redirect to #{redirect_url}"
+    sina.redirect redirect_url, return_code
+    # return "Danke"
+  end
+
+# --- private functions ---
+private
+
+  def get_redirect_config(host)
+    if ! $config[host]
+      write_log "LOADING #{host}.yaml"
+      $config[host] = load_yaml "#{host}.yaml"
+    end
+    $config[host]
+  end
+
+  def load_yaml(filename)
+    if File.file? filename
+        begin
+          YAML::load_file(filename)
+        rescue StringIndexOutOfBoundsException => e
+          puts "Error: YAML parsing in #{filename}"
+          write_log "Error: YAML parsing in #{filename}"
+          write_log e.message
+          raise "YAML not parsable"
+          false
+        rescue Exception => e
+          puts "Error: YAML parsing in #{filename}"
+          write_log "Error: YAML parsing in #{filename}"
+          write_log e.message
+          raise "YAML not parsable"
+          false
+        end
+    else
+      raise "File not found: #{filename}"
+    end
+  end
+
+  def write_log(message)
+    puts message
+  end
+
+end
